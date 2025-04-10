d92f9717d3b3e32f5e1700fffeec3aa2377b5ab4
Ehsan.Aj <eng.ahanj@gmail.com>
2025-04-10 15:46:11 +0330

    some logic3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Cookie, Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
 
